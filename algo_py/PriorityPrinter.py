857f0c61bbf6b1bfafce9b2ceb107b14d6b2a1e4
rwh <fkrns1744@naver.com>
2021-02-09 18:21:28 +0900

    [Solved : 프린터]

--- algo_py/PriorityPrinter.py
+++ algo_py/PriorityPrinter.py
@@ -6,15 +6,17 @@ def solution(priorities, location):
     dq = deque(priorities)
 
     while len(dq):
-        if dq[0] == max(dq):    # 첫 값이 가장 우선순위가 높은 경우
+        if dq[0] == max(dq):  # 우선순위 체크
             answer += 1
-            dq.popleft()        # 바로 뺌
-            if location == 0:   # 타겟 인덱스 0, 우선 순위가 가장 높다면 뺌
+            dq.popleft()
+            #  location 체크
+            if location == 0:
                 return answer
             else:
-                location -= 1   # 아니라면 값이 하나 빠졌으니 타겟 인덱스 감소
+                location -= 1
         else:
-            dq.append(dq.popleft())
+            dq.append(dq.popleft())  # 우선순위가 높지 않다면 뒤로 보냄
+            # location 체크, 다른 점은 0번이면 끝으로 갔을테니 최종 위치에서 하나씩 감소
             if location == 0:
                 location = len(dq) - 1
             else:
